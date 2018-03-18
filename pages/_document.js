8a312e9043573fd884716db83171b52b2ad3e7ba
James <james.colton@gmail.com>
2018-03-18 19:43:00 +0000

    apollo global wrap

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,30 @@
 import * as React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document'
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'https://mpjk0plp9.lp.gql.zone/graphql' }),
+  cache: new InMemoryCache(),
+});
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <html lang="en">
-        <Head>
-          <link rel="stylesheet" href="/_next/static/style.css" />
-        </Head>
-        <body>
-          <Main />
-          eeeeeeeeeeeeeeeeee
+      <ApolloProvider client={client}>
+        <html lang="en">
+          <Head>
+            <link rel="stylesheet" href="/_next/static/style.css" />
+          </Head>
+          <body>
+            <Main />
+            eeeeeeeeeeeeeeeeee
                     <NextScript />
-        </body>
-      </html>
+          </body>
+        </html>
+      </ApolloProvider>
     )
   }
 }
