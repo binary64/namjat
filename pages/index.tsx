b0198fd898db3df806e71afd67cb7232aca825dc
James <james.colton@gmail.com>
2018-03-20 05:22:45 +0000

    react-sticky-el

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
-import gql from 'graphql-tag';
-import * as React from 'react';
-import { graphql } from 'react-apollo';
+import gql from 'graphql-tag'
+import * as React from 'react'
+import { graphql } from 'react-apollo'
 import { withData } from '../lib/apollo'
 import Container from '../containers/Main'
-import { ProgressBar } from 'primereact/components/progressbar/ProgressBar';
-import { Layout, Menu, Icon, Button } from 'antd';
+import { ProgressBar } from 'primereact/components/progressbar/ProgressBar'
+import { Layout, Menu, Icon, Button } from 'antd'
 //import Sticky from '../components/Sticky'
-import { StickyContainer, Sticky } from 'react-sticky';
+//import { StickyContainer, Sticky } from 'react-sticky-plus'
+import Sticky from 'react-sticky-el';
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Footer, Sider, Content } = Layout
 
 type User = {
   lastName: string
@@ -41,21 +42,13 @@ export default class extends React.Component {
     )
     return (
       <Container>
-        <StickyContainer>
-          <Sticky title='wooohh' style={{}}>
-            {
-              () => {
-                return (
-                  <h1>Header wow</h1>
-                )
-              }
-            }
-          </Sticky>
+        <Sticky>
+          <h1>Header wow</h1>
+        </Sticky>
 
-          {
-            this.props.data.allUsers.map((e, i) => (<li key={i}>{e.firstName}</li>))
-          }
-        </StickyContainer>
+        {
+          this.props.data.allUsers.map((e, i) => (<li key={i}>{e.firstName}</li>))
+        }
 
       </Container>
     )
