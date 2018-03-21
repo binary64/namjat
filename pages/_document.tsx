d5e2ad1acea8173d78c70d689ae6119afbba4a34
James <james.colton@gmail.com>
2018-03-21 05:38:24 +0000

    prune

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,8 +9,6 @@ export default class MyDocument extends Document {
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <meta charSet='utf-8' />
           <link rel='stylesheet' href='https://unpkg.com/antd@3/dist/antd.min.css' />
-          <link rel='stylesheet' href='https://unpkg.com/primereact/resources/themes/omega/theme.css' />
-          <link rel='stylesheet' href='https://unpkg.com/primereact/resources/primereact.min.css' />
         </Head>
         <body>
           <Main />
